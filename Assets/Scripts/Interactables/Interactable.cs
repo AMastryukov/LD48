4e33cb97d39dcd992fb9db7c1a37e0f2dea20803
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2021-04-24 12:03:00 -0400

    Add Interactables with cooldown & custom unity events

---
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private float cooldown = 0f;
+    public InteractableEvent OnInteracted;
+
+    private float currentCooldown = 0f;
+
+    public void Interact()
+    {
+        if (currentCooldown > 0f)
+        {
+            Debug.Log("COOLDOWN: " + currentCooldown);
+            return;
+        }
+
+        Debug.Log("INTERACTED!");
+
+        OnInteracted?.Invoke();
+
+        currentCooldown = cooldown;
+    }
+
+    private void Update()
+    {
+        if (currentCooldown > 0f)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+
+        if (currentCooldown < 0f)
+        {
+            currentCooldown = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+	[SerializeField] private float maxUpAngle = 45f;
+	[SerializeField] private float maxDownAngle = 25f;
+
+	private float speed = 1f;
+	private Vector2 rotation = Vector2.zero;
+
+    private void Start()
+    {
+		speed = PlayerPrefs.GetFloat("MouseSensitivity", 1f);
+    }
+
+    private void Update()
+	{
+		PollCameraInput();
+		PollInteractionInput();
+	}
+
+	private void PollCameraInput()
+    {
+		rotation.y += Input.GetAxis("Mouse X") * speed;
+		rotation.x += -Input.GetAxis("Mouse Y") * speed;
+
+		rotation.x = Mathf.Clamp(rotation.x, -maxUpAngle, maxDownAngle);
+
+		transform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0f);
+	}
+
+	private void PollInteractionInput()
+    {
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+		{
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				if (hit.collider.GetComponent<Interactable>() != null)
+                {
+					hit.collider.GetComponent<Interactable>().Interact();
+                }
+			}
+		}
+    }
+}
