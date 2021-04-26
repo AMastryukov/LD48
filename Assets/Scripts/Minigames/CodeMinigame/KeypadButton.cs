ee68496ae8e2d45737ee73d84a61327b0347a295
Zuhair <zuhair.ghias@hotmail.com>
2021-04-26 13:36:09 -0400

    Add clicking sounds

--- Assets/Scripts/Minigames/CodeMinigame/KeypadButton.cs
+++ Assets/Scripts/Minigames/CodeMinigame/KeypadButton.cs
@@ -6,12 +6,14 @@ public class KeypadButton : Interactable
 {
 
     [SerializeField] CodeMinigame codegame;
-
+    private AudioSource src;
+    private bool isPressed;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        isPressed = false;
+        src = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,21 @@ public class KeypadButton : Interactable
 
     public void Pressed(string s)
     {
+        if (isPressed) return;
+        src.pitch = Random.Range(0.8f, 1.2f);
+        src.Play();
         codegame.EnterKey(s);
+        StartCoroutine(AnimatePress());
+    }
+
+    private IEnumerator AnimatePress()
+    {
+        isPressed = true;
+        Vector3 original = transform.localPosition;
+        transform.localPosition = original + Vector3.right*0.03f;
+        yield return new WaitForSeconds(0.2f);
+        transform.localPosition = original;
+        isPressed = false;
     }
 
 
